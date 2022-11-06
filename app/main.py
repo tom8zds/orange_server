dae797bced97179772862d4169615fcf4ba18820
tomzds8 <2407164659@qq.com>
2022-11-06 20:58:13 +0800

    init search api

--- app/main.py
+++ app/main.py
@@ -6,11 +6,19 @@ tmdb.API_KEY = api_key
 
 app = FastAPI()
 
+def write_log(message: str):
+    with open("log.txt", mode="a") as log:
+        log.write(message)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/search/{bangumi_name}")
-async def read_item(bangumi_name):
-    return {"bangumi_name": bangumi_name}
+async def read_item(bangumi_name:str):
+    assert(len(bangumi_name) > 3)
+    search = tmdb.Search()
+    response = search.tv (query=bangumi_name, language="zh", include_adult=True)
+    # for s in search.results:
+    #     write_log(s['title'], s['id'], s['release_date'], s['popularity'])
+    return response
