ed6eadd2b3da6d42efe95dc3c8e854f37e1556a7
tomzds8 <2407164659@qq.com>
2022-11-14 22:06:27 +0800

    implement mikan source timetable

---
+++ orange/source/mikan/mikan_source.py
@@ -0,0 +1,122 @@
+from http import HTTPStatus
+import re
+from typing import List
+from orange.source.model.bangumi import Bangumi
+from orange.source.model.time_table import Season, TimeTable, WeekDay
+from orange.source.source import AbstractSource
+
+import bs4
+
+import requests
+from urllib.parse import quote
+
+MIKAN_BASE = 'https://mikanani.me'
+MIKAN_SEASON = '/Home/BangumiCoverFlowByDayOfWeek'
+
+_SeasonData = {
+    Season.SPRING: {
+        "ch": "春"
+    },
+    Season.SUMMER: {
+        "ch": "夏"
+    },
+    Season.AUTUMN: {
+        "ch": "秋"
+    },
+    Season.WINTER: {
+        "ch": "冬"
+    },
+    Season.OTHER: {
+        "ch": "OVA"
+    }
+}
+
+_WeekDay = {
+    '星期六': WeekDay.SAT,
+    '星期日': WeekDay.SUN,
+    '星期一': WeekDay.MON,
+    '星期二': WeekDay.TUE,
+    '星期三': WeekDay.WED,
+    '星期四': WeekDay.THU,
+    '星期五': WeekDay.FRI,
+    '剧场版': WeekDay.OTH,
+    'OVA': WeekDay.OTH,
+}
+
+
+class MikanSource(AbstractSource):
+
+    def update() -> None:
+        pass
+
+    def get_time_table(year: int, season: Season) -> TimeTable:
+        pass
+
+    def parse_season(self, text: str) -> dict[WeekDay, List[Bangumi]]:
+
+        result: dict[WeekDay, List[Bangumi]] = {}
+
+        soup = bs4.BeautifulSoup(text, "html.parser")
+        rowElements = soup.find_all("div", attrs={"class": "sk-bangumi"})
+
+        for row in rowElements:
+            titleElem = row.find("div", attrs={"class": "row"})
+            if (titleElem != None):
+                print(titleElem.text)
+                day = re.sub('\s+', '', titleElem.text)
+                weekDay: WeekDay = _WeekDay[day]
+                anime_list = row.find_all("li")
+
+                bangumi_list: List[Bangumi] = []
+                for anime in anime_list:
+                    cover = anime.find("span").attrs["data-src"]
+                    # cover = anime.find("span").attributes("background-image")
+                    anime_info = anime.find("div", attrs={"class": "an-info"})
+                    anime_info_group = anime_info.find(
+                        "div", attrs={"class": "an-info-group"})
+
+                    if (anime_info_group.find("a") is None):
+                        print("null")
+                        anime_name = anime_info_group.find_all("div")[
+                            1].attrs["title"]
+                    else:
+                        anime_name = anime_info_group.find("a").attrs["title"]
+
+                    anime_id = anime.find("span").attrs["data-bangumiid"]
+
+                    bangumi: Bangumi = Bangumi(anime_id, anime_name, cover)
+
+                    print([anime_id, anime_name, cover])
+                    bangumi_list.append(bangumi)
+
+                result[weekDay] = bangumi_list
+        return result
+
+    def query_season(self, year: int, season: Season) -> tuple[bool, TimeTable]:
+
+        param = "?year={}&seasonStr={}".format(
+            year, quote(_SeasonData.get(season).get("ch")))
+
+        url = MIKAN_BASE + MIKAN_SEASON + param
+        print(url)
+
+        payload = {}
+        headers = {}
+
+        try:
+            response = requests.request("GET", url, headers=headers, data=payload)
+            
+            if(response.status_code == HTTPStatus.OK):
+                data = self.parse_season(response.text)
+                result = TimeTable(year, season, data)
+                return [True, result]
+        except Exception as err:
+            print(f"Unexpected {err=}, {type(err)=}")
+            return [False, err]
+            
+        return [False, None]
+        
+            
+
+
+         
