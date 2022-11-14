30a2c15801ccf99f3cc4b930a0cb4ebdf96e5456
tomzds8 <2407164659@qq.com>
2022-11-14 22:04:44 +0800

    delete

--- test.py
+++
@@ -1,8 +0,0 @@
-from app.dev_config import api_key
-import tmdbsimple as tmdb
-tmdb.API_KEY = api_key
-
-search = tmdb.Search()
-response = search.tv (query='bangumi_name')
-for s in search.results:
-    print(s['title'], s['id'], s['release_date'], s['popularity'])
